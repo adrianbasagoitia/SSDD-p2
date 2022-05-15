107a7391e8e2e2bf975a073e1d6a30d5fec69d4e
My Name <myemail@example.com>
2022-05-15 18:40:51 +0200

    Added basic structure of API REST for bycicle reserve

---
+++ p2-rest-bycicle-reserve/src/main/java/es/urjc/sd/p2/P2RestBycicleReserveApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.sd.p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2RestBycicleReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2RestBycicleReserveApplication.class, args);
+	}
+
+}
