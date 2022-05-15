e7440e6101dc34d5ca7baa81d51cc10994e03e5c
My Name <myemail@example.com>
2022-05-15 18:32:00 +0200

    Added basic structure of API REST dor users

---
+++ p2-rest-user/src/test/java/es/urjc/sd/p2/P2RestUserApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.sd.p2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2RestUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
